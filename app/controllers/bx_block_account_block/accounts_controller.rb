6449c4b6f40b677bf15a23083cf4f1c2b6e8cdb5
anil divaker <anildivaker87@gmail.com>
2024-08-14 16:08:57 +0530

    modify verify otp api resend otp api

--- app/controllers/bx_block_account_block/accounts_controller.rb
+++ app/controllers/bx_block_account_block/accounts_controller.rb
@@ -1,10 +1,11 @@
 module BxBlockAccountBlock
 	class AccountsController < ApplicationController
+		skip_before_action :verify_authenticity_token
 		def create
       case params[:data][:type]
       when 'email_account'
         account_params = params.require(:data).require(:attributes).permit(:first_name, :last_name, :email, :password)
-        query_email = account_params[:email].downcase
+        query_email = account_params[:email]
         account = EmailAccount.where('LOWER(email) = ?', query_email).first
         user_account = BxBlockAccountBlock::Account.where(email: account_params[:email])
         if user_account.present? && user_account.pluck(:activated).include?(true)
@@ -27,33 +28,42 @@ module BxBlockAccountBlock
       end
     end
 
-    def verify_email_otp
-      account = BxBlockAccountBlock::Account.find_by(email: params[:data][:attributes][:email])
-      email_otp = EmailOtp.find_by(email: account.email)
-      if email_otp&.pin == params[:data][:attributes][:otp].to_i && email_otp.valid_until > Time.current
-        account.update(activated: true)
-        render json: { message: "OTP verified, account activated", user: account.as_json.merge(token: request.headers[:token]) }, status: :ok
-      else
-        render json: { message: "Invalid or expired OTP" }, status: :unprocessable_entity
-      end
-    end
+		def verify_email_otp
+			account = BxBlockAccountBlock::Account.find_by(email: params[:data][:attributes][:email])
+			if account.present?
+				if account.activated
+					render json: { message: "Account is already activated" }, status: :unprocessable_entity
+				else
+					email_otp = EmailOtp.find_by(email: account.email)
+					if email_otp&.pin == params[:data][:attributes][:otp].to_i && email_otp.valid_until > Time.current
+						account.update(activated: true)
+						render json: { message: "OTP verified, account activated", user: account.as_json.merge(token: request.headers[:token]) }, status: :ok
+					else
+						render json: { message: "Invalid or expired OTP" }, status: :unprocessable_entity
+					end
+				end
+			else
+				render json: { message: "Account not found" }, status: :not_found
+			end
+		end
 
-		def resend_email_otp
+	  def resend_email_otp
 			account = BxBlockAccountBlock::Account.find_by(email: params[:data][:attributes][:email])
 			if account.present?
 				otp_token = generate_otp
 				valid_until = 5.minutes.from_now
-				email_otp = EmailOtp.create!(
-					email: account.email,
-					pin: otp_token,
-					valid_until: valid_until
-				)
+				email_otp = EmailOtp.find_by(email: account.email)
+				if email_otp.present?
+					email_otp.update(pin: otp_token, valid_until: valid_until)
+				else
+					EmailOtp.create!(email: account.email, pin: otp_token, valid_until: valid_until)
+				end
 				send_otp_email(account.email, otp_token)
 				render json: { message: 'OTP has been resent' }, status: :created
 			else
 				render json: { errors: [{ message: 'Account not found' }] }, status: :not_found
 			end
-		end
+	  end
 
 		def index
 			@account = BxBlockAccountBlock::Account.all 
