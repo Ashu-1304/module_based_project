a52e915e1775a88b4f5774780b3cb78f034c9e96
John Doe <you@example.com>
2024-08-14 15:58:47 +0530

    correction of token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,24 @@
 class ApplicationController < ActionController::Base
-    protect_from_forgery with: :null_session
+  before_action :authenticate_request
+  protect_from_forgery with: :null_session
+
+  private
+
+  def authenticate_request
+    header = request.headers["token"]
+    token = header.split(" ").last if header
+
+    if token
+      begin
+        @decoded = BuilderJsonWebToken::JsonWebToken.decode(token)
+        @current_user = BxBlockAccountBlock::Account.find(@decoded[:user_id])
+      rescue ActiveRecord::RecordNotFound => e
+        render json: { errors: "Account not found: #{e.message}" }, status: :unauthorized
+      rescue JWT::DecodeError => e
+        render json: { errors: "Invalid token: #{e.message}" }, status: :unauthorized
+      end
+    else
+      render json: { errors: "Missing token" }, status: :unauthorized
+    end
+  end
 end
